50fdcbc248758905efd722b40ec9d9853922ddc4
Max Wunderlich <mwunderlich@11931L.local>
2013-05-19 22:09:39 -0700

    forms & validation, most of

---
+++ forms.py
@@ -0,0 +1,8 @@
+from flask.ext.wtf import Form, TextField, TextAreaField, SubmitField, validators, ValidationError
+
+class ContactForm(Form):
+  name = TextField("Name", [validators.Required()])
+  email = TextField("Email", [validators.Required(), validators.Email()])
+  subject = TextField("Subject", [validators.Required()])
+  message = TextAreaField("Message", [validators.Required()])
+  submit = SubmitField("Send")
--- routes.py
+++ routes.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+from forms import ContactForm
 
 app = Flask(__name__)
+
+app.secret_key = 'ancestral recall'
 
 @app.route('/')
 def home():
@@ -9,6 +12,20 @@ def home():
 @app.route('/about')
 def about():
   return render_template('about.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+	form = ContactForm()
+
+	if request.method == "POST":
+		if form.validate() == False:
+		  flash('All fields are required.')
+		  return render_template('contact.html', form=form)
+		else:
+		  return 'Form posted.'
+
+	elif request.method == "GET":
+		return render_template('contact.html', form=form)
 
 if __name__ == '__main__':
   app.run(debug=True)
